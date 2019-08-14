5ea3f946e1dbf953c26c8a04b57bd854a5eff78d
james ekene <edehjamesraphael@gmail.com>
2019-08-14 07:39:04 -0700

    Updates

---
+++ precache-manifest.0bad408b782c9b5f5e0ebcba05217ae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e2791e7efd7fabc1abe51bb39a1a41",
+    "url": "/monster-rollodex/index.html"
+  },
+  {
+    "revision": "a55bdcd8b8fc7a6d5185",
+    "url": "/monster-rollodex/static/css/main.af96d0c7.chunk.css"
+  },
+  {
+    "revision": "6d9732ae4975475a2c61",
+    "url": "/monster-rollodex/static/js/2.f536d6a4.chunk.js"
+  },
+  {
+    "revision": "a55bdcd8b8fc7a6d5185",
+    "url": "/monster-rollodex/static/js/main.8dbe72b1.chunk.js"
+  },
+  {
+    "revision": "094b07d7f2413853765d",
+    "url": "/monster-rollodex/static/js/runtime~main.40875546.js"
+  }
+]);
